83357384f90cc508ca4d5963e4c4705296e03b47
Evgeny <73193661+EVG-cmd@users.noreply.github.com>
2022-07-24 15:11:58 +0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "resource_group_name_prefix" {
+  default       = "rg"
+  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "resource_group_location" {
+  default       = "eastus"
+  description   = "Location of the resource group."
+}
+variable "azurerm_linux_virtual_machine" {
+  admin_user = "evgeny"
+}
